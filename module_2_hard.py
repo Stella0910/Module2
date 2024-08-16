db38c86194c170b74571292474926d6b422c4eed
Stella0910 <aniretake0910@gmail.com>
2024-08-16 23:58:31 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,6 +1,3 @@
-import random
-
-
 def get_numbers():
     for j in range(len(list_) - 1):
         sum_ = list_[0] + list_[j + 1]
@@ -15,7 +12,6 @@ while True:
     n = int(input('Введите число от 3 до 20: '))
     if n in stones1:
         result = []
-        print(n)
         list_ = list(range(1, n))
         for i in range(len(list_)):
             if len(list_) > 1:
